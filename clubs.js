713215e4a63b467a6d60dad7dab5bb38b9e27cad
Nicholas Rees <nicholas.rees@live.co.uk>
2016-12-11 22:22:47 +0000

    2015/16 data and clubs

---
+++ clubs.js
@@ -0,0 +1,125 @@
+DATA.clubs = {
+  "name": "English Premier League 2015/16",
+  "clubs": [
+    {
+      "key": "chelsea",
+      "name": "Chelsea",
+      "code": "CHE",
+      "icon": "Chelsea.ico"
+    },
+    {
+      "key": "arsenal",
+      "name": "Arsenal",
+      "code": "ARS",
+      "icon": "Arsenal.ico"
+    },
+    {
+      "key": "tottenham",
+      "name": "Tottenham Hotspur",
+      "code": "TOT",
+      "icon": "Tottenham Hotspur.ico"
+    },
+    {
+      "key": "westham",
+      "name": "West Ham United",
+      "code": "WHU",
+      "icon": "West Ham United.ico"
+    },
+    {
+      "key": "crystalpalace",
+      "name": "Crystal Palace",
+      "code": "CRY",
+      "icon": "Crystal Palace.ico"
+    },
+    {
+      "key": "manutd",
+      "name": "Manchester United",
+      "code": "MUN",
+      "icon": ""
+    },
+    {
+      "key": "mancity",
+      "name": "Manchester City",
+      "code": "MCI",
+      "icon": "Manchester United.ico"
+    },
+    {
+      "key": "everton",
+      "name": "Everton",
+      "code": "EVE",
+      "icon": "Everton.ico"
+    },
+    {
+      "key": "liverpool",
+      "name": "Liverpool",
+      "code": "LIV",
+      "icon": "Liverpool FC.ico"
+    },
+    {
+      "key": "westbrom",
+      "name": "West Bromwich Albion",
+      "code": "WBA",
+      "icon": "West Bromwich Albion.ico"
+    },
+    {
+      "key": "newcastle",
+      "name": "Newcastle United",
+      "code": "NEW",
+      "icon": "Newcastle United.ico"
+    },
+    {
+      "key": "stoke",
+      "name": "Stoke City",
+      "code": "STK",
+      "icon": "Stoke City.ico"
+    },
+    {
+      "key": "sunderland",
+      "name": "Sunderland",
+      "code": "SUN",
+      "icon": "Sunderland.ico"
+    },
+    {
+      "key": "astonvilla",
+      "name": "Aston Villa",
+      "code": "AVL",
+      "icon": "Aston Villa.ico"
+    },
+    {
+      "key": "southampton",
+      "name": "Southampton",
+      "code": "SOU",
+      "icon": "Southampton FC.ico"
+    },
+    {
+      "key": "leicester",
+      "name": "Leicester City",
+      "code": "LEI",
+      "icon": "Leicester City.ico"
+    },
+    {
+      "key": "bournemouth",
+      "name": "Bournemouth",
+      "code": "BOU",
+      "icon": "AFC Bournemouth.ico"
+    },
+    {
+      "key": "norwich",
+      "name": "Norwich City",
+      "code": "NOR",
+      "icon": "Norwich City.ico"
+    },
+    {
+      "key": "watford",
+      "name": "Watford",
+      "code": "WAT",
+      "icon": "Watford FC.ico"
+    },
+    {
+      "key": "swansea",
+      "name": "Swansea City",
+      "code": "SWA",
+      "icon": "Swansea City.ico"
+    }
+  ]
+}
